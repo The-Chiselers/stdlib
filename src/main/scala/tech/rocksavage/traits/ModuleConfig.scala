f988de8fae151b70d31453d3aedb007a5b4ce49b
Nick Allison <nickrallison1@gmail.com>
2025-01-11 15:31:07 -0700

    added scaladocs

--- src/main/scala/tech/rocksavage/traits/ModuleConfig.scala
+++ src/main/scala/tech/rocksavage/traits/ModuleConfig.scala
@@ -2,6 +2,18 @@
 // This code is licensed under the Apache Software License 2.0 (see LICENSE.MD)
 package tech.rocksavage.traits
 
+/**
+ * A trait that defines the interface for module configurations.
+ * Implementations of this trait should provide a method to retrieve default configurations
+ * as a `Map[String, Any]`, where the keys are configuration names and the values are the
+ * corresponding configuration parameters.
+ */
 trait ModuleConfig {
+  /**
+   * Retrieves the default configurations for the module.
+   *
+   * @return A `Map[String, Any]` containing the default configurations, where the keys are
+   *         configuration names and the values are the corresponding configuration parameters.
+   */
   def getDefaultConfigs: Map[String, Any]
 }
--- src/main/scala/tech/rocksavage/util/Util.scala
+++ src/main/scala/tech/rocksavage/util/Util.scala
@@ -6,7 +6,18 @@ import java.io.{ByteArrayOutputStream, PrintWriter}
 import scala.util.Using
 import sys.process._
 
+/**
+ * Utility object providing helper methods for system operations, such as finding executables
+ * in the system's PATH and running shell commands.
+ */
 object Util {
+
+  /**
+   * Searches for an executable in the system's PATH.
+   *
+   * @param program The name of the executable to search for.
+   * @return An `Option[String]` containing the full path to the executable if found, or `None` if not found.
+   */
   def which(program: String): Option[String] = {
     val envPath  = sys.env("PATH")
     val pathList = envPath.split(java.io.File.pathSeparator).toList
@@ -20,6 +31,15 @@ object Util {
     resultUnix.orElse(resultWindows)
   }
 
+  /**
+   * Runs a shell command and captures its output and exit status.
+   *
+   * @param cmd A sequence of strings representing the command and its arguments.
+   * @return A tuple containing:
+   *         - The exit status of the command (Int).
+   *         - The standard output of the command as a String.
+   *         - The standard error of the command as a String.
+   */
   def runCommand(cmd: Seq[String]): (Int, String, String) = {
     val stdout = new StringBuilder
     val stderr = new StringBuilder
