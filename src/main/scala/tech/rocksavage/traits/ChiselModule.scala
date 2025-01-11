a83f23f4d203025a7c76c4cfd93ae985959eed2a
Nick Allison <nickrallison1@gmail.com>
2025-01-11 14:09:32 -0700

    adding configtrait

--- src/main/scala/tech/rocksavage/traits/ChiselModule.scala
+++
@@ -1,7 +0,0 @@
-package tech.rocksavage
-
-import chisel3.RawModule
-
-trait ChiselModule extends RawModule {
-  def defaultConfigs: Map[String, Any]
-}
---
+++ src/main/scala/tech/rocksavage/traits/ConfigTrait.scala
@@ -0,0 +1,5 @@
+package tech.rocksavage.config
+
+trait ConfigTrait {
+  def getDefaultConfigs: Map[String, Any]
+}
